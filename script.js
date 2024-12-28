fc8495a7a6a7200a32e546a68f3a4d9db7d06712
AlieDocAtaur <atauraahmad@gmail.com>
2024-12-28 23:51:34 +0100

    box & hover effect added

--- script.js
+++ script.js
@@ -1,71 +1,34 @@
-
-// const container = document.createElement('div');
-
-// container.classList.add('container');
-// console.log(container);
-// for (let i = 0; i < 17; i++) {
-//     let div = document.createElement('div')
-//     div.classList.add('grid-square')
-//     div.innerHTML = 'YO!'
-//     // console.log(div.innerText);
-//     container.appendChild(div)
-// }
-
-// var dom = document.implementation.createDocument('http://www.w3.org/1999/xhtml', 'html', null);
-// var body = dom.createElement("body");
-// dom.documentElement.appendChild(body);
-
-// set timeout is needed because document.body is created after the current continuation finishes
-// setTimeout(function() {    
-//   document.body.appendChild(container)
-// },0)
-
 const container = document.querySelector('.container')
-const btn = document.querySelector('.btn')
+let containerWidth = container.offsetWidth
 
+console.log(containerWidth);
 
-// for (let i = 0; i <= 16; i++) {
-//     let div = document.createElement('div')
-//     div.classList.add('square-grid')
-//     container.appendChild(div)
-
-//     div.addEventListener('mouseover', ()=>{
-//         // div.classList.add('redBG')
-//         div.style.backgroundColor = 'red'
-//     })
-//     div.addEventListener('mouseout',()=>{
-//         div.style.background = 'black'
-//     })
-// }
-
-function createDivs() {
-    for (let i = 0; i <= 16; i++) {
-        let div = document.createElement('div')
-        div.classList.add('square-grid')
-        container.appendChild(div)
+function createSquares(numberOfSquares) {
     
-        div.addEventListener('mouseover', ()=>{
-            // div.classList.add('redBG')
-            div.style.backgroundColor = 'red'
+    let noOfC = numberOfSquares
+    let squareSize = 960/noOfC
+    
+    for (let i = 0; i < noOfC * noOfC; i++) {
+        let span = document.createElement('div')
+        span.classList.add('box');
+        span.style.height = `${squareSize}px`;
+        span.style.width = ` ${squareSize}px`;
+        container.appendChild(span)   
+        
+        span.addEventListener('mouseover',()=>{
+            span.style.background = ' blue'
         })
-        div.addEventListener('mouseout',()=>{
-            div.style.background = 'black'
+
+        span.addEventListener('mouseout',()=>{
+            span.style.background = ' white'
         })
+
     }
 }
 
-document.addEventListener('DOMContentLoaded', createDivs)
 
-btn.addEventListener('click', ()=> {
-    let newSize = prompt('Squares size? ')
-    console.log(newSize);
-    container.innerHTML = ''
-    let newSquareGrid = HTMLAllCollection.createElement('span')
-    newSquareGrid.classList.add('newSquareDrid')
-    container.appendChild(newSquareGrid)
-})
-
-
+createSquares(16)
+// console.log(document.body.offsetWidth);
 
 
 
